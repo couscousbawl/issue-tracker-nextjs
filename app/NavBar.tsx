da22f62066293a63a635673095cf6018aacdbef0
Faserly App <85167495+faserly@users.noreply.github.com>
2023-10-26 16:28:52 +0300

    Style active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -15,8 +21,12 @@ const NavBar = () => {
       <ul className='flex space-x-6'>
         {links.map(link => 
         <Link 
-          key={link.href} href="/" 
-          className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+          key={link.href} href={link.href} 
+          className={classnames({
+            'text-zinc-900' : link.href === currentPath,
+            'text-zinc-500' : link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors': true
+          })}>
             {link.label}
         </Link>
           )}
